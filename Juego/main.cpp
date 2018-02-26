7fe4ebf3a815068ffff1f0896a9f3ef32c34ce5c
Laia <36154539+LaiaMartinezMotis@users.noreply.github.com>
2018-02-26 15:19:32 +0100

    Add files via upload

---
+++ Juego/main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "../SDL/include/SDL.h"
+
+#pragma comment(lib, "../SDL/libx86/SDL2.lib")
+#pragma comment(lib, "../SDL/libx86/SDL2main.lib")
+
+int main(int argc, char*argv[]) {
+	printf("Hello world");
+	system("pause");
+		return 0;
+}
